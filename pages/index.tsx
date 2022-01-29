e4caf880c9712a14c37891bee00d7021ad70d989
RichCode <chineduogada@gmail.com>
2022-01-29 06:12:36 +0100

    test(HomPage): have test passing

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Layout from "components/Layout/Layout";
 
 const HomePage: NextPage = () => {
-  return <Layout>Hi</Layout>;
+  return <Layout>Hello</Layout>;
 };
 
 export default HomePage;
