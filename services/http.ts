2af0c367bc3fcb2a06715a8081554693d5f4aa4e
RichCode <chineduogada@gmail.com>
2022-01-31 08:14:45 +0100

    refacor(Http): have a working http module
    Refs: #6

---
+++ services/http.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+export const baseURL = "https://jsonplaceholder.typicode.com";
+
+const defaultOptions = () => ({
+  // timeout's the request in 2 minute by default
+  timeout: 60 * 2 * 1000,
+});
+
+const buildOptions = (options: { [x: string]: any }) => ({
+  ...defaultOptions(),
+  ...options,
+});
+const buildURL = (path: string) => {
+  return baseURL + path;
+};
+
+export const http = {
+  get: (path: string, options: { [x: string]: any }) =>
+    axios.get(options?.url || buildURL(path), buildOptions(options)),
+  post: (
+    path: string,
+    data: { [x: string]: any },
+    options: { [x: string]: any }
+  ) => axios.post(options?.url || buildURL(path), data, buildOptions(options)),
+  patch: (
+    path: string,
+    data: { [x: string]: any },
+    options: { [x: string]: any }
+  ) => axios.patch(options?.url || buildURL(path), data, buildOptions(options)),
+  delete: (path: string, options: { [x: string]: any }) =>
+    axios.delete(options?.url || buildURL(path), buildOptions(options)),
+};
